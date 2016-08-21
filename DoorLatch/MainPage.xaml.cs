db5acf6143f88acf1c174fe4255241b448ac9a98
Rich Hopkins <hopmedic@hotmail.com>
2016-08-21 02:45:06 -0400

    Changed design, moved to one Pi per door plan.

--- DoorLatch/MainPage.xaml.cs
+++ DoorLatch/MainPage.xaml.cs
@@ -4,6 +4,9 @@ using Windows.UI.Xaml.Controls;
 using Windows.Devices.I2c;
 using Windows.Devices.Enumeration;
 using System.Threading;
+using Windows.Devices.Gpio;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -17,13 +20,60 @@ namespace DoorLatch
 	{
 		private I2cDevice device;
 		private Timer periodicTimer;
+		private DispatcherTimer lightTimer;
 		private bool lightOn = false;
 		private bool initialized = false;
-		
+		private GpioPin bit0Pin;
+		private GpioPin bit1Pin;
+		private GpioPin doorPin;
+		private GpioPinValue pinValue;
+		private bool gpioStatus = false;
+
 		public MainPage()
 		{
 			this.InitializeComponent();
 			initCommunication();
+			InitGpio();
+			InitLightTimer();
+		}
+
+		private void InitGpio()
+		{
+			var gpio = GpioController.GetDefault();
+			if (gpio == null)
+			{
+				return;
+			}
+			bit0Pin = gpio.OpenPin(20);
+			bit0Pin.SetDriveMode(GpioPinDriveMode.Output);
+			bit0Pin.Write(GpioPinValue.Low);
+			bit1Pin = gpio.OpenPin(19);
+			bit1Pin.SetDriveMode(GpioPinDriveMode.Output);
+			bit1Pin.Write(GpioPinValue.Low);
+			doorPin = gpio.OpenPin(21);
+			doorPin.SetDriveMode(GpioPinDriveMode.Output);
+			doorPin.Write(GpioPinValue.Low);
+		}
+
+		private void InitLightTimer()
+		{
+			lightTimer = new DispatcherTimer();
+			lightTimer.Interval = TimeSpan.FromMilliseconds(4000);
+			lightTimer.Tick += LightTimer_Tick;
+			lightTimer.Stop();
+		}
+
+		private void LightTimer_Tick(object sender, object e)
+		{
+			(sender as DispatcherTimer).Stop();
+			bit0Pin.Write(GpioPinValue.Low);
+			bit1Pin.Write(GpioPinValue.Low);
+			doorPin.Write(GpioPinValue.Low);
+			var task = this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+			{
+				txtBadgeNum.Text = "";
+				txtTimeStamp.Text = "";
+			});
 		}
 
 		private async void initCommunication()
@@ -35,7 +85,7 @@ namespace DoorLatch
 				string aqs = I2cDevice.GetDeviceSelector("I2C1");
 				var dis = await DeviceInformation.FindAllAsync(aqs);
 				device = await I2cDevice.FromIdAsync(dis[0].Id, settings);
-				periodicTimer = new Timer(this.TimerCallback, null, 0, 1000);
+				periodicTimer = new Timer(this.TimerCallback, null, 0, 500);
 				initialized = true;
 			}
 			catch (Exception e)
@@ -46,74 +96,88 @@ namespace DoorLatch
 
 		private void TimerCallback(object state)
 		{
-			if (!initialized)
-			{
-				initCommunication();
-				return;
-			}
+			//if (!initialized)
+			//{
+			//	initCommunication();
+			//	return;
+			//}
 			byte[] RegAddrBuf = new byte[] { 8 };
-			byte[] ReadBuf = new Byte[11];
-			char[] cArray = new char[11];
+			byte[] ReadBuf = new Byte[8];
+			char[] cArray = new char[8];
 			try
 			{
 				device.Read(ReadBuf);
-				cArray = System.Text.Encoding.UTF8.GetString(ReadBuf, 0, 11).ToCharArray();
+				if (ReadBuf[0] == 0 && ReadBuf[1] == 255 && ReadBuf[2] == 255 && ReadBuf[3] == 255 && ReadBuf[4] == 255 &&
+					ReadBuf[5] == 255 && ReadBuf[6] == 255 && ReadBuf[7] == 255) return;
+				if (ReadBuf[0] == 70 && ReadBuf[1] == 70 && ReadBuf[2] == 70 && ReadBuf[3] == 70 && ReadBuf[4] == 70 &&
+					ReadBuf[5] == 70 && ReadBuf[6] == 70 && ReadBuf[7] == 70) return;
+				cArray = System.Text.Encoding.UTF8.GetString(ReadBuf, 0, 8).ToCharArray();
+				if (cArray[0] == 'F')
+				{
+
+				}
 			}
 			catch (Exception f)
 			{
 				Debug.WriteLine(f.Message);
 			}
 
-			if (cArray[0] == '0' && cArray.Length == 11)
-			{
-				
+
 			string str = new string(cArray);
 			Debug.WriteLine(str);
 
 			var task = this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
 			{
-				txtBadgeNum.Text = str.ToString().Substring(2);
+				txtBadgeNum.Text = str.ToString();
 				DateTime time = DateTime.Now;
 				string format = "hh:mm:ss tt";
 				txtTimeStamp.Text = time.ToString(format);
 			});
 			ParseData(str);
-			//Timer blankTimer = new Timer(this.BlankForm, null, TimeSpan.FromSeconds(5).Milliseconds, Timeout.Infinite);
-			}
+
 		}
 
 		private void BlankForm(object state)
 		{
-			var task = this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-			{
-				txtBadgeNum.Text = "";
-				txtTimeStamp.Text = "";
-			});
 		}
 
-		private void ParseData(string badgeDoorData)
+		private async void ParseData(string badgeNum)
 		{
-			string doorNum = badgeDoorData.Substring(1, 2);
-			string badgeNum = badgeDoorData.Substring(3);
 			if (badgeNum == "6014EAD5")
 			{
+				bit0Pin.Write(GpioPinValue.High);
 				SetAccessGranted(true);
-				SendData("1" + doorNum + badgeNum);
 			}
 			else
 			{
 				SetAccessGranted(false);
-				SendData("0" + doorNum + badgeNum);
 			}
+			await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+			{
+				lightTimer.Start();
+			});
 		}
 
 		private void SetAccessGranted(bool accessGranted)
 		{
 			var task = this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
 			{
-				if (accessGranted) txtAccessGranted.Text = "Yes";
-				else txtAccessGranted.Text = "No";
+				if (accessGranted)
+				{
+					txtAccessGranted.Text = "Yes";
+					bit0Pin.Write(GpioPinValue.High);
+					bit1Pin.Write(GpioPinValue.High);
+					doorPin.Write(GpioPinValue.High);
+				}
+				else
+				{
+					txtAccessGranted.Text = "No";
+					bit0Pin.Write(GpioPinValue.High);
+					bit1Pin.Write(GpioPinValue.Low);
+					doorPin.Write(GpioPinValue.Low);
+				}
 			});
+
 		}
 
 		private void SendData(string authorization)
--- RFIDDoorLatch/RFIDDoorLatch.ino
+++ RFIDDoorLatch/RFIDDoorLatch.ino
@@ -9,15 +9,15 @@ const int DoorID = 8;
 const int redPin = 3; 
 const int bluePin = 5;  
 const int greenPin = 6;
-const int doorPin = 2; 
-String strDoorID;
+const int bit0Pin = 7;
+const int bit1Pin = 8;
 
 // Color Modes
 const int COLOR_MODE_OFF = 0;
 const int COLOR_MODE_RED = 1;
 const int COLOR_MODE_GREEN = 2;
 const int COLOR_MODE_BLUE = 3;
-const int COLOR_MODE_YELLOW = 4;
+const int COLOR_MODE_PURPLE = 4;
 
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 MFRC522::MIFARE_Key key;
@@ -33,26 +33,26 @@ void setup()
 	Wire.begin(DoorID);
 	Serial.begin(115200);
 	SPI.begin();
-	rfid.PCD_Init();
-
-	strDoorID = String(DoorID);
-	while (strDoorID.length() < 2) {
-		strDoorID = "0" + strDoorID;
-	}
+	rfid.PCD_Init(); //Initialize the RFID reader
 
 	pinMode(redPin, OUTPUT);
 	pinMode(greenPin, OUTPUT);
 	pinMode(bluePin, OUTPUT);
-	pinMode(doorPin, OUTPUT);
+	pinMode(bit0Pin, INPUT);
+	pinMode(bit1Pin, INPUT);
 	digitalWrite(redPin, LOW);
 	digitalWrite(greenPin, LOW);
 	digitalWrite(bluePin, LOW);
-	digitalWrite(doorPin, LOW);
+
 
 	time = millis();
 
 	setLEDMode(COLOR_MODE_BLUE);
-	Serial.println("Door # " + strDoorID + " initialized");
+
+	Serial.print("Door # ");
+	Serial.print(DoorID);
+	Serial.println(" initialized.");
+
 	clearTagData();
 	Wire.onRequest(SendData);
 	Wire.onReceive(ReceiveData);
@@ -60,6 +60,8 @@ void setup()
 
 void loop()
 {
+	CheckLEDInput();
+
 	if (!hasTag) {
 		CheckForCard();
 	}
@@ -68,6 +70,28 @@ void loop()
 		if (time2 - time > 3000 || time2 < time) {
 			clearTagData();
 		}
+	}
+}
+
+void CheckLEDInput() {
+	int bit0 = digitalRead(bit0Pin);
+	int bit1 = digitalRead(bit1Pin);
+
+	//standby
+	if (bit0 == LOW && bit1 == LOW) {
+		setLEDMode(COLOR_MODE_BLUE);
+		return;
+	}
+
+	//granted
+	if (bit0 == HIGH && bit1 == HIGH) {
+		setLEDMode(COLOR_MODE_GREEN);
+		return;
+	}
+
+	//denied
+	if (bit0 == HIGH && bit1 == LOW) {
+		setLEDMode(COLOR_MODE_RED);
 	}
 }
 
@@ -92,13 +116,71 @@ void loop()
 		printHex(rfid.uid.uidByte, rfid.uid.size);
 		hasTag = true;
 		time = millis();
-		setLEDMode(COLOR_MODE_YELLOW);
+		//setLEDMode(COLOR_MODE_PURPLE);
 
 		//halt PICC
 		rfid.PICC_HaltA();
 		//stop encryption
 		rfid.PCD_StopCrypto1();
 		cardSent = false;
+	}
+
+	void clearTagData() {
+		Serial.println("clearTagData()");
+		tag = "FFFFFFFFFFF";
+		cardSent = false;
+		hasTag = false;
+		//setLEDMode(COLOR_MODE_BLUE);
+	}
+
+
+	void printHex(byte *buffer, byte bufferSize) {
+		tag = "";
+		for (byte i = 0; i < bufferSize; i++) {
+			tag += String(buffer[i], HEX);
+		}
+		tag.toUpperCase();
+		Serial.print("tag variable is : ");
+		Serial.print(tag);
+		Serial.println();
+	}
+
+	void SendData() {
+		if (!cardSent) {
+			Serial.println("Sending Data :  " + tag);
+			Wire.write(tag.c_str());
+			cardSent = true;
+		}
+	}
+
+	void ReceiveData(int bytes) {
+		//char cArr[(bytes + 1)]; 
+		//int index = 0;
+		//while (Wire.available()) {
+		//	cArr[index] = Wire.read();
+		//	index++;
+		//}
+		//String receivedData = String(cArr);
+		//receivedData = receivedData.substring(0, 11);
+		//Serial.println("Received Data : " + receivedData);
+
+		///*if (receivedData.substring(1) != tag.substring(1)) {
+		//	return;
+		//}*/
+		//if (receivedData.substring(0, 1) == "1" && receivedData.substring(1) == tag.substring(1)) {
+		//	GrantAccess();
+		//}
+		//else {
+		//	DenyAccess();
+		//}
+	}
+
+	void GrantAccess() {
+		setLEDMode(COLOR_MODE_GREEN);
+	}
+
+	void DenyAccess() {
+		setLEDMode(COLOR_MODE_RED);
 	}
 
 	void setLEDMode(int color) {
@@ -127,8 +209,8 @@ void loop()
 			digitalWrite(bluePin, HIGH);
 		}
 
-		//YELLOW
-		if (color == COLOR_MODE_YELLOW) {
+		//PURPLE
+		if (color == COLOR_MODE_PURPLE) {
 			analogWrite(redPin, 127);
 			analogWrite(greenPin, 127);
 			analogWrite(bluePin, 127);
@@ -141,66 +223,5 @@ void loop()
 			digitalWrite(bluePin, LOW);
 		}
 
-	}
-
-	void clearTagData() {
-		Serial.println("clearTagData()");
-		tag = "FFFFFFFFFFF";
-		cardSent = false;
-		hasTag = false;
-		digitalWrite(doorPin, LOW);
-		setLEDMode(COLOR_MODE_BLUE);
-	}
-
-
-	void printHex(byte *buffer, byte bufferSize) {
-		tag = "0";
-		tag += strDoorID;
-		for (byte i = 0; i < bufferSize; i++) {
-			tag += String(buffer[i], HEX);
-		}
-		tag.toUpperCase();
-		Serial.print("tag variable is : ");
-		Serial.print(tag);
-		Serial.println();
-	}
-
-	void SendData() {
-		if (!cardSent) {
-			Serial.println("Sending Data :  " + tag);
-			Wire.write(tag.c_str());
-			cardSent = true;
-		}
-	}
-
-	void ReceiveData(int bytes) {
-		char cArr[(bytes + 1)]; //					TRY REMOVING + 1
-		int index = 0;
-		while (Wire.available()) {
-			cArr[index] = Wire.read();
-			index++;
-		}
-		String receivedData = String(cArr);
-		receivedData = receivedData.substring(0, 11);
-		Serial.println("Received Data : " + receivedData);
-
-		/*if (receivedData.substring(1) != tag.substring(1)) {
-			return;
-		}*/
-		if (receivedData.substring(0, 1) == "1" && receivedData.substring(1) == tag.substring(1)) {
-			GrantAccess();
-		}
-		else {
-			DenyAccess();
-		}
-	}
-
-	void GrantAccess() {
-		setLEDMode(COLOR_MODE_GREEN);
-		digitalWrite(doorPin, HIGH);
-	}
-
-	void DenyAccess() {
-		setLEDMode(COLOR_MODE_RED);
 	}
 
